e5334ae523cc0bc65909a77f18653abb75effff6
dan <dan@foohq.io>
2025-07-18 07:46:53 +0200

    devbox: Maintain a list of modules compiled in by default
    Add test function in devbox.sh and pass the list of modules to
    `go test`.
    
    Load the module list in Windows test runner.

--- devbox.sh
+++ devbox.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env sh
 
+export MODULES="$(tr '\n'  ' ' < default_modules.txt)"
+
+test() {
+    if [ -z "$TAGS" ]; then
+        TAGS="$MODULES"
+    fi
+
+    export CGO_ENABLED=1
+    go test -tags "$TAGS" -race ./...
+}
+
 build() {
     if [ -z "$OUTPUT" ]; then
         OUTPUT="build/ren"
+    fi
+
+    if [ -z "$TAGS" ]; then
+        TAGS="$MODULES"
     fi
 
     go build -tags "$TAGS" -o "$OUTPUT" ./cmd/ren
